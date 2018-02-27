471591f69da4dbd74aaac3fb7dd13ef80fb74760
Dacer <qqwerrd@gmail.com>
2018-02-27 14:43:57 +0800

    fix OTA bug on Android Oero

--- sdk/src/main/java/com/hackplan/theairsdk/UpdateService.java
+++ sdk/src/main/java/com/hackplan/theairsdk/UpdateService.java
@@ -1,37 +1,56 @@
 package com.hackplan.theairsdk;
 
-import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import android.widget.Toast;
 
-public class UpdateService extends IntentService {
-    private static final int BUFFER_SIZE = 10 * 1024; // 8k ~ 32K
-    private static final String TAG = "UpdateService";
+import com.liulishuo.filedownloader.BaseDownloadTask;
+import com.liulishuo.filedownloader.FileDownloadListener;
+import com.liulishuo.filedownloader.FileDownloader;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UpdateService extends Service {
     private NotificationManager mNotifyManager;
     private NotificationCompat.Builder mBuilder;
+    private final static int NOTI_ID = 23498;
+    private final static String NOTI_CHANNEL_ID = "the_air";
 
-    public UpdateService() {
-        super("UpdateService");
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
-    @Override protected void onHandleIntent(Intent intent) {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String urlStr = intent.getStringExtra(Constants.APK_DOWNLOAD_URL);
+        FileDownloader.setup(this);
+        File dir = getExternalCacheDir();
+        String apkName = urlStr.substring(urlStr.lastIndexOf("/") + 1, urlStr.length());
+        final File apkFile = new File(dir, apkName);
+
 
         mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mBuilder = new NotificationCompat.Builder(this, "1");
+        mBuilder = new NotificationCompat.Builder(this, NOTI_CHANNEL_ID);
 
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTI_CHANNEL_ID,
+                    "OTA", NotificationManager.IMPORTANCE_HIGH);
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
         mBuilder.setContentTitle(
                 getString(R.string.downloading_with_app_name, MyUtils.getApplicationName(this)))
                 .setSmallIcon(android.R.drawable.stat_sys_download)
@@ -40,98 +59,91 @@ public class UpdateService extends IntentService {
                 .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(),
                         PendingIntent.FLAG_CANCEL_CURRENT))
                 .setOngoing(true);
-        mNotifyManager.notify(0, mBuilder.build());
-        String urlStr = intent.getStringExtra(Constants.APK_DOWNLOAD_URL);
-        InputStream in = null;
-        FileOutputStream out = null;
-        try {
-            URL url = new URL(urlStr);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        mNotifyManager.notify(NOTI_ID, mBuilder.build());
 
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setDoOutput(false);
-            urlConnection.setConnectTimeout(10 * 1000);
-            urlConnection.setReadTimeout(10 * 1000);
-            urlConnection.setRequestProperty("Connection", "Keep-Alive");
-            urlConnection.setRequestProperty("Charset", "UTF-8");
-            urlConnection.setRequestProperty("Accept-Encoding", "gzip, deflate");
+        FileDownloader.getImpl().create(urlStr)
+                .setPath(apkFile.getPath())
+                .setForceReDownload(true)
+                .setListener(new FileDownloadListener() {
+                    @Override
+                    protected void pending(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                        if (totalBytes != 0) updateProgress(soFarBytes * 100 / totalBytes);
+                    }
 
-            urlConnection.connect();
-            long byteTotal = urlConnection.getContentLength();
-            long byteSum = 0;
-            int byteRead;
-            in = urlConnection.getInputStream();
-            File dir = getExternalCacheDir();
-            String apkName = urlStr.substring(urlStr.lastIndexOf("/") + 1, urlStr.length());
-            File apkFile = new File(dir, apkName);
-            out = new FileOutputStream(apkFile);
-            byte[] buffer = new byte[BUFFER_SIZE];
+                    @Override
+                    protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                        if (totalBytes != 0) updateProgress(soFarBytes * 100 / totalBytes);
+                    }
 
-            int oldProgress = 0;
+                    @Override
+                    protected void completed(BaseDownloadTask task) {
+                        Intent installAPKIntent = new Intent(Intent.ACTION_VIEW);
+                        String[] command = { "chmod", "777", apkFile.toString() };
+                        ProcessBuilder builder = new ProcessBuilder(command);
+                        try {
+                            builder.start();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
 
-            while ((byteRead = in.read(buffer)) != -1) {
-                byteSum += byteRead;
-                out.write(buffer, 0, byteRead);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            installAPKIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            Uri contentUri = TheAirFileProvider.getUriForFile(UpdateService.this,
+                                    getPackageName() + ".com.hackplan.fileprovider", apkFile);
+                            installAPKIntent.setDataAndType(contentUri,
+                                    "application/vnd.android.package-archive");
+                        } else {
+                            installAPKIntent.setDataAndType(Uri.fromFile(apkFile),
+                                    "application/vnd.android.package-archive");
+                            installAPKIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        }
 
-                int progress = (int) (byteSum * 100L / byteTotal);
-                if (progress != oldProgress) {
-                    updateProgress(progress);
-                }
-                oldProgress = progress;
-            }
+                        startActivity(installAPKIntent);
+                        PendingIntent pendingIntent = PendingIntent.getActivity(UpdateService.this, 0, installAPKIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
 
-            Intent installAPKIntent = new Intent(Intent.ACTION_VIEW);
-            String[] command = { "chmod", "777", apkFile.toString() };
-            ProcessBuilder builder = new ProcessBuilder(command);
-            builder.start();
+                        mBuilder.setContentText(getString(R.string.download_success))
+                                .setProgress(0, 0, false)
+                                .setContentIntent(pendingIntent)
+                                .setOngoing(false);
+                        Notification notification = mBuilder.build();
+                        notification.flags = Notification.FLAG_AUTO_CANCEL;
+                        mNotifyManager.notify(NOTI_ID, notification);
+                        stopSelf();
+                    }
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                installAPKIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                Uri contentUri = TheAirFileProvider.getUriForFile(this,
-                        getPackageName() + ".com.hackplan.fileprovider", apkFile);
-                installAPKIntent.setDataAndType(contentUri,
-                        "application/vnd.android.package-archive");
-            } else {
-                installAPKIntent.setDataAndType(Uri.fromFile(apkFile),
-                        "application/vnd.android.package-archive");
-                installAPKIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            }
+                    @Override
+                    protected void paused(BaseDownloadTask task, int soFarBytes, int totalBytes) {
 
-            startActivity(installAPKIntent);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, installAPKIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
+                    }
 
-            mBuilder.setContentText(getString(R.string.download_success))
-                    .setProgress(0, 0, false)
-                    .setContentIntent(pendingIntent)
-                    .setOngoing(false);
-            Notification notification = mBuilder.build();
-            notification.flags = Notification.FLAG_AUTO_CANCEL;
-            mNotifyManager.notify(0, notification);
-        } catch (Exception e) {
-            Log.e(TAG, "download apk file error", e);
-        } finally {
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException ignored) {
-                }
-            }
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException ignored) {
-                }
-            }
-        }
+                    @Override
+                    protected void error(BaseDownloadTask task, Throwable e) {
+                        mNotifyManager.cancel(NOTI_ID);
+                        Toast.makeText(UpdateService.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        stopSelf();
+                    }
+
+                    @Override
+                    protected void warn(BaseDownloadTask task) {
+
+                    }
+                })
+                .start();
+
+        return START_REDELIVER_INTENT;
     }
 
+    /**
+     *
+     * @param progress 0 - 100
+     */
     private void updateProgress(int progress) {
         mBuilder.setContentText(this.getString(R.string.download_progress, progress))
                 .setProgress(100, progress, false);
         PendingIntent pendingintent =
                 PendingIntent.getActivity(this, 0, new Intent(), PendingIntent.FLAG_CANCEL_CURRENT);
         mBuilder.setContentIntent(pendingintent);
-        mNotifyManager.notify(0, mBuilder.build());
+        mNotifyManager.notify(NOTI_ID, mBuilder.build());
     }
 }
